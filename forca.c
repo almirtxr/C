5bb6efa6a4b680644ccb5d82b6e1267e843a7404
Almir Gabriel do Santo Teixeira <almir.gabriel@oabms.local>
2024-05-10 15:22:45 -0400

    primeiro commit

--- forca.c
+++ forca.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+	char palavra[20];
 
+	sprintf(palavra, "MELANCIA");
+
+	int acertou = 0;
+	int enforcou = 0;
+
+	do {
+		char chute;
+		scanf("%c", &chute);
+
+		for (int i = 0; i < strlen(palavra); i++) {
+			if (palavra[i] == chute) {
+				printf("A posição %d tem essa letra! \n", i);
+			}
+		}
+		while (!acertou && !enforcou);
+	}
+
+}
